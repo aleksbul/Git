c2e0238d24f2efa2abc71f5ce39f22285e63dc00
aleksandra <alexandra.bulgak@gmail.com>
2023-01-19 12:53:17 +0200

    task

---
+++ Seminar_Task/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число");
+double number = double.Parse(Console.ReadLine());
+int pow = 0;
+
+// pow - степень числа 2 при которой значение 2 в степени pow максимально близко к заданному числу, но не больше его.
+
+for (int i = 0; Math.Pow(2, i) <= number; i++)
+{
+    pow = i;
+}
+
+int size = pow + 1;
+int[] array = new int[size];
+
+
+for (int i = 0; i < size; i++)
+{
+    if (number >= Math.Pow(2, pow))
+    {
+        array[i] = 1;
+        number = number - Math.Pow(2, pow);
+    }
+    else array[i] = 0;    
+    pow = pow - 1;
+}
+
+Console.WriteLine($"{String.Join("", array)}");
